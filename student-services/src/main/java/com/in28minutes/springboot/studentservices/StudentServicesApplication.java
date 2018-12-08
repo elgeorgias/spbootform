eca33c2d1dc6f1e544e0bcdc08a6dbba535a19ac
Jorge RM <jorge.ruano.murillo@gmail.com>
2018-12-08 18:24:15 +0100

    Second commit
    - modif on system out

--- student-services/src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
+++ student-services/src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentServicesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Web Spring Boot");
+		System.out.println("Starting Web Spring Boot - initializing");
 		SpringApplication.run(StudentServicesApplication.class, args);
 	}
 }
